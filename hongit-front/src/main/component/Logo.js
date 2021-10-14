f634f96a5ba778b9acb982eb68286e4ea9885871
JiHongKim15 <tsopime@gmail.com>
2021-10-14 16:59:34 +0900

    modfied

--- hongit-front/src/main/component/Logo.js
+++ hongit-front/src/main/component/Logo.js
@@ -3,14 +3,10 @@ import logo from './hongit_logo.png';
 import React, { Component } from 'react';
 
 class Logo extends Component {
-    logoClick = () => {
-        console.log('clicked');
-    }
-
     render() {
         return (
             <div >
-                <img src={logo} className="Main-logo" alt="logo" onClick = {this.logoClick}/>
+                <img src={logo} className="Main-logo" alt="logo"/>
             </div>
         );
     }
